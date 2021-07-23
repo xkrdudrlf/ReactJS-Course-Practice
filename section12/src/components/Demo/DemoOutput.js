0ed4f54f9d78828c5e4970b019746c2b76c556b6
Tom Tak <y0ung-gil@hanmail.net>
2021-07-23 16:11:29 +0900

    section 13 complete

--- section12/src/components/Demo/DemoOutput.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const DemoOutput = (props) => {
-  console.log("Demo Output");
-  return <p>{props.show ? "This is new" : ""}</p>;
-};
-
-export default React.memo(DemoOutput);
