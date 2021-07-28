07726192798e0a7e45bc765d631a9e9d54c29c31
Tom Tak <y0ung-gil@hanmail.net>
2021-07-28 19:14:50 +0900

    section 18 complete

---
+++ section18/src/App.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
+
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  return (
+    <Fragment>
+      <Header />
+      {isAuthenticated ? <UserProfile /> : <Auth />}
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
